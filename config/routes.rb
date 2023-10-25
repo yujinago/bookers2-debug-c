ee577d0ad5ffbca0995b372dee7506a1eba17a5b
yujinago <138287741+yujinago@users.noreply.github.com>
2023-10-25 00:07:21 +0000

    add-controller&route

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
   
   resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
     resource :group_users, only: [:create, :destroy]
+    resources :event_notices, only: [:new, :create]
+    get "event_notices" => "event_notices#sent"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
